2a21f080bda60b3cf87ef68c43b9f9d6e30c16df
Zimlewis <z1ml3w1s123@gmail.com>
2025-05-23 22:32:18 +0700

    fix package problem

--- src/components/MobileHeroBanner.tsx
+++ src/components/MobileHeroBanner.tsx
@@ -1,7 +1,5 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectCoverflow } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/effect-coverflow";
 import { FaPlayCircle } from "react-icons/fa";
 
 interface MovieItem {
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "swiper/css";
 import { Inspector } from "react-dev-inspector";
 import App from "./App.tsx";
 
