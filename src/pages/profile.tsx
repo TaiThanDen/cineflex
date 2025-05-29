5186134ef74ca744bf720ca28bed12f88366a02a
AIM-9M <anhductruong151@gmail.com>
2025-05-29 16:25:39 +0700

    remove unused imports

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,9 +1,6 @@
 import { useState } from "react";
 import {
   // PlusIcon,
-  UserIcon,
-  ClockIcon,
-  HomeIcon,
   ArrowLeftStartOnRectangleIcon,
 } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
