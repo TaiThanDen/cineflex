6303e2e7888d50d6568fd93001130caa50c95fa3
Zimlewis <z1ml3w1s123@gmail.com>
2025-07-25 09:21:42 +0700

    fix import

--- src/components/data/User.ts
+++
@@ -1,9 +0,0 @@
- export interface User {
-     id: string;
-     name: string;
-     role: "User" | "Admin" | "Moderator" | "UserVip";
-     email: string;
-     phone: string;
-     status: "Active" | "Banned";
-     profile: string;
- }
