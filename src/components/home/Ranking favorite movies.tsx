baaac2b0ce59f540d27ffdae2863156660e68cd6
Shilily <tai25062006z@gmail.com>
2025-08-22 16:12:22 +0700

    favorite

---
+++ src/components/home/Ranking favorite movies.tsx
@@ -0,0 +1,12 @@
+
+
+const RankingFavoriteMovies = () => {
+    return (
+        <div>
+            <h2>Ranking Favorite Movies</h2>
+            {/* Add your ranking logic here */}
+        </div>
+    );
+};
+
+export default RankingFavoriteMovies;
